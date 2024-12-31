9862bb86742dd0e7e2de442dfcdc500d77fe1ba3
AKIBBB <aaakibmonir@gmail.com>
2025-01-01 03:53:52 +0600

    main

--- logout.js
+++ logout.js
@@ -1,22 +1,29 @@
 function handleLogout() {
+    const token = localStorage.getItem('token');
+    
+    if (!token) {
+        alert('No token found, you are not logged in.');
+        return;
+    }
+
     fetch('https://elisiyan.onrender.com/users/logout/', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Token ${localStorage.getItem('token')}`  
+            'Authorization': `Token ${token}`  
         }
     })
-        .then(response => {
-            if (response.ok) {
-                localStorage.removeItem('token');
-                alert('Logged out successfully');
-                window.location.href = 'index.html'; 
-            } else {
-                alert('Logout failed');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('An error occurred');
-        });
+    .then(response => {
+        if (response.ok) {
+            localStorage.removeItem('token');
+            alert('Logged out successfully');
+            window.location.href = 'index.html'; 
+        } else {
+            alert('Logout failed');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred');
+    });
 }
